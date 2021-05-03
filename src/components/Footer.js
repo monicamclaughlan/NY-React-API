99106ef9be43f1fa86dd791586da6be616e3ef8a
Monica McLaughlan <monicamclaughlan@MacBook-Air.local>
2021-05-03 14:45:11 -0400

    updated link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = (props) => {
     return (
         <>
-            <div>gR</div>
+            <Link to="/"><div>gR</div></Link>
             <p>Copyright 2021</p>
             <p className="back-to-top" onClick={() => window.scrollTo({
                 top: 0,
