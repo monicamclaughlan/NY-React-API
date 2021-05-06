a1ae9341c39bf8ac19dc3e4bdfe9950a10dfce7f
Monica McLaughlan <monicamclaughlan@MacBook-Air.local>
2021-05-06 15:10:38 -0400

    cleaned up code in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,16 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 const Form = ({handleChange}) => {
-    // const [search, setSearch] = useState('')
 
-    // const handleChange = (event) => { 
-    //     console.log('handleChange - event', event.target.value)
-    //     const name = event.target.value
-    //     setSearch({ 
-    //         name
-    //     })
-    //} 
 
     return (
         <form>
             <input
             onChange={handleChange}
-            // value={search.title}
             type="text"
             placeholder="Search by Title/Author"
             />
-            {/* <button
-            onClick={(e) => props.handleSubmit(e,search.name)}
-            >Submit</button> */}
+            
         </form>
     )
 }
